0bf06f3051db67645182f4563f76ba1e981f6bf3
1HusniddinUz <husniddinhalimov112@gmail.com>
2025-10-13 16:51:54 +0500

    preview img updated

---
+++ src/locales/translationsEn.js
@@ -0,0 +1,21 @@
+const translationsUz = {
+    headersec: "Home page",
+    about: "About Us",
+    products: `Clothes`,
+    order: "To Order",
+    contact: `Contact Us`,
+    toOrder: "To Order",
+    quotes: `“Oriental beauty reinvented for the modern woman.”`,
+    aboutUs: `"MALIKA SULTAN dresses are a unique union of tradition and modernity. Every woman who wears them feels not just beautiful, but also powerful like a queen. Because each dress is a work of art woven from handwork, warmth of the heart, and national pride.`,
+    collectionBtn: "Clothing collection",
+    location: `Location `,
+    openHour: "Open Hours",
+    generalManager: `CEO: `,
+    managerName: `Djureyeva Gulchehra Narzulloyevna`,
+    womenSuit: `Ladies' suit`,
+    womenBags: `Ladies' bag`,
+    phone: `Phone: `,
+    email: `Email: `,
+};
+
+export default translationsUz;
